a679606717db0c25bdee2ac50b3da873edbaadc7
Parviz <mirpar1998@yandex.ru>
2018-04-04 23:16:55 +0700

    commit

---
+++ IKIT GO/Assets/Scripts/ScrollTrees.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTrees : MonoBehaviour {
+
+	public float speed = -2f;
+
+	void Update(){
+		transform.Translate (speed * Time.deltaTime, 0f, 0f);
+
+		if (transform.position.x <= -10f) {
+			transform.Translate (20f, 0f, 0f);
+		}
+	}
+}
